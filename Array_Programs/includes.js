3d2272aff60965b166226cb7b7be6047e85fe66a
sachin <sba40599@gmail.com>
2024-01-08 23:55:50 +0530

    .

---
+++ Array_Programs/includes.js
@@ -0,0 +1,9 @@
+let array=[1,2,3,4,5,6,7];
+let targetValue=10;
+for(let i=0;i<array.length;i++){
+    if(array[i]==targetValue)
+    console.log(`true`);
+    else
+    console.log(`false`);
+    break
+}
--- Array_Programs/removeElement.js
+++ Array_Programs/removeElement.js
@@ -4,4 +4,18 @@ let arr=[1,2,34,5];
 //console.log(arr);
 //arr.shift()
 let result=delete arr[0]
-console.log(result);
+//console.log(result);
+
+
+//remove element using for loop
+let array=[1,2,3,4,5];
+let finalArray=[];
+let removeEle=4;
+
+for(let i =0;i<array.length;i++) {
+    if(array[i] !== removeEle){
+        finalArray.push(array[i]);
+    }
+}
+console.log(finalArray);
+
